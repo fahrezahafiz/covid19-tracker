439b2c46df93d0858ac6286edf0b420909484657
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-05-15 17:11:04 +0700

    create country_view.dart

---
+++ lib/ui/views/country_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CountryView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Country View\nCountry: '),
+      ),
+    );
+  }
+}
