18eb95d25933be29b5d258031f40b8736e1c3eac
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-05-15 17:10:28 +0700

    Create router.dart

---
+++ lib/ui/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:covid19tracker/ui/views/all_views.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomeView());
+      case '/country':
+        return MaterialPageRoute(builder: (_) => CountryView());
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          );
+        });
+    }
+  }
+}
